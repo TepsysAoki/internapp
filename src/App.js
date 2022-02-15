42dbdb56e6f3d784ad6498bd5bac5128c0c9b92a
EC2 Default User <ec2-user@ip-172-31-0-14.ap-northeast-1.compute.internal>
2022-02-15 11:27:32 +0000

    in chatbot

--- src/App.js
+++ src/App.js
@@ -5,90 +5,260 @@ import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 import { listNotes } from './graphql/queries';
 import { createNote as createNoteMutation, deleteNote as deleteNoteMutation } from './graphql/mutations';
 
+import {
+  Box,
+  Button,
+  CssBaseline,
+  Divider,
+  Link,
+  ThemeProvider,
+  Typography,
+  createTheme,
+} from '@mui/material';
+import {
+  ActionRequest,
+  AudioActionResponse,
+  ChatController,
+  FileActionResponse,
+  MuiChat,
+} from 'chat-ui-react';
 
-const initialFormState = { name: '', description: '' }
+const muiTheme = createTheme({
+  palette: {
+    primary: {
+      main: '#007aff',
+    },
+  },
+});
 
-function App() {
-  const [notes, setNotes] = useState([]);
-  const [formData, setFormData] = useState(initialFormState);
+export function App(): React.ReactElement {
+  const [chatCtl] = React.useState(
+    new ChatController({
+      showDateTime: true,
+    }),
+  );
 
-  useEffect(() => {
-    fetchNotes();
-  }, []);
-
-  async function onChange(e) {
-    if (!e.target.files[0]) return
-    const file = e.target.files[0];
-    setFormData({ ...formData, image: file.name });
-    await Storage.put(file.name, file);
-    fetchNotes();
-  }
-  
-  async function fetchNotes() {
-    const apiData = await API.graphql({ query: listNotes });
-    const notesFromAPI = apiData.data.listNotes.items;
-    await Promise.all(notesFromAPI.map(async note => {
-      if (note.image) {
-        const image = await Storage.get(note.image);
-        note.image = image;
-      }
-      return note;
-    }))
-    setNotes(apiData.data.listNotes.items);
-  }
-
-   async function createNote() {
-    if (!formData.name || !formData.description) return;
-    await API.graphql({ query: createNoteMutation, variables: { input: formData } });
-    if (formData.image) {
-      const image = await Storage.get(formData.image);
-      formData.image = image;
-    }
-    setNotes([ ...notes, formData ]);
-    setFormData(initialFormState);
-  }
-
-  async function deleteNote({ id }) {
-    const newNotesArray = notes.filter(note => note.id !== id);
-    setNotes(newNotesArray);
-    await API.graphql({ query: deleteNoteMutation, variables: { input: { id } }});
-  }
+  React.useMemo(() => {
+    echo(chatCtl);
+  }, [chatCtl]);
 
   return (
-    <div className="App">
-      <h1>My Notes App</h1>
-      <input
-        onChange={e => setFormData({ ...formData, 'name': e.target.value})}
-        placeholder="Note name"
-        value={formData.name}
-      />
-      <input
-        onChange={e => setFormData({ ...formData, 'description': e.target.value})}
-        placeholder="Note description"
-        value={formData.description}
-      />
-      <input
-        type="file"
-        onChange={onChange}
-      />
-      <button onClick={createNote}>Create Note</button>
-      <div style={{marginBottom: 30}}>
-        {
-          notes.map(note => (
-            <div key={note.id || note.name}>
-              <h2>{note.name}</h2>
-              <p>{note.description}</p>
-              <button onClick={() => deleteNote(note)}>Delete note</button>
-              {
-                note.image && <img src={note.image} style={{width: 400}} />
-              }
-            </div>
-          ))
-        }
-      </div>
-      <AmplifySignOut />
-    </div>
+    <ThemeProvider theme={muiTheme}>
+      <CssBaseline />
+      <Box sx={{ height: '100%', backgroundColor: 'gray' }}>
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            height: '100%',
+            maxWidth: '640px',
+            marginLeft: 'auto',
+            marginRight: 'auto',
+            bgcolor: 'background.default',
+          }}
+        >
+          <Typography sx={{ p: 1 }}>
+            Welcome to{' '}
+            <Link href="https://github.com/twihike/chat-ui-react">
+              chat-ui-react
+            </Link>{' '}
+            demo site.
+          </Typography>
+          <Divider />
+          <Box sx={{ flex: '1 1 0%', minHeight: 0 }}>
+            <MuiChat chatController={chatCtl} />
+          </Box>
+        </Box>
+      </Box>
+    </ThemeProvider>
   );
 }
 
-export default withAuthenticator(App);
+async function echo(chatCtl: ChatController): Promise<void> {
+  await chatCtl.addMessage({
+    type: 'text',
+    content: `Please enter something.`,
+    self: false,
+    avatar: '-',
+  });
+  const text = await chatCtl.setActionRequest({
+    type: 'text',
+    placeholder: 'Please enter something',
+  });
+  await chatCtl.addMessage({
+    type: 'text',
+    content: `You have entered:\n${text.value}`,
+    self: false,
+    avatar: '-',
+  });
+
+  await chatCtl.addMessage({
+    type: 'text',
+    content: `What is your gender?`,
+    self: false,
+    avatar: '-',
+  });
+  const sel = await chatCtl.setActionRequest({
+    type: 'select',
+    options: [
+      {
+        value: 'man',
+        text: 'Man',
+      },
+      {
+        value: 'woman',
+        text: 'Woman',
+      },
+      {
+        value: 'other',
+        text: 'Other',
+      },
+    ],
+  });
+  await chatCtl.addMessage({
+    type: 'text',
+    content: `You have selected ${sel.value}.`,
+    self: false,
+    avatar: '-',
+  });
+
+  await chatCtl.addMessage({
+    type: 'text',
+    content: `What is your favorite fruit?`,
+    self: false,
+    avatar: '-',
+  });
+  const mulSel = await chatCtl.setActionRequest({
+    type: 'multi-select',
+    options: [
+      {
+        value: 'apple',
+        text: 'Apple',
+      },
+      {
+        value: 'orange',
+        text: 'Orange',
+      },
+      {
+        value: 'none',
+        text: 'None',
+      },
+    ],
+  });
+  await chatCtl.addMessage({
+    type: 'text',
+    content: `You have selected '${mulSel.value}'.`,
+    self: false,
+    avatar: '-',
+  });
+
+  await chatCtl.addMessage({
+    type: 'text',
+    content: `What is your favorite picture?`,
+    self: false,
+    avatar: '-',
+  });
+  const file = (await chatCtl.setActionRequest({
+    type: 'file',
+    accept: 'image/*',
+    multiple: true,
+  }));
+  await chatCtl.addMessage({
+    type: 'jsx',
+    content: (
+      <div>
+        {file.files.map((f) => (
+          <img
+            key={file.files.indexOf(f)}
+            src={window.URL.createObjectURL(f)}
+            alt="File"
+            style={{ width: '100%', height: 'auto' }}
+          />
+        ))}
+      </div>
+    ),
+    self: false,
+    avatar: '-',
+  });
+
+  await chatCtl.addMessage({
+    type: 'text',
+    content: `Please enter your voice.`,
+    self: false,
+    avatar: '-',
+  });
+  const audio = (await chatCtl
+    .setActionRequest({
+      type: 'audio',
+    })
+    .catch(() => ({
+      type: 'audio',
+      value: 'Voice input failed.',
+      avatar: '-',
+    })));
+  await (audio.audio
+    ? chatCtl.addMessage({
+        type: 'jsx',
+        content: (
+          <a href={window.URL.createObjectURL(audio.audio)}>Audio downlaod</a>
+        ),
+        self: false,
+        avatar: '-',
+      })
+    : chatCtl.addMessage({
+        type: 'text',
+        content: audio.value,
+        self: false,
+        avatar: '-',
+      }));
+
+  await chatCtl.addMessage({
+    type: 'text',
+    content: `Please press the button.`,
+    self: false,
+    avatar: '-',
+  });
+  const good = await chatCtl.setActionRequest({
+    type: 'custom',
+    Component: GoodInput,
+  });
+  await chatCtl.addMessage({
+    type: 'text',
+    content: `You have pressed the ${good.value} button.`,
+    self: false,
+    avatar: '-',
+  });
+
+  echo(chatCtl);
+}
+
+function GoodInput({
+  chatController,
+  actionRequest,
+}: {
+  chatController: ChatController;
+  actionRequest: ActionRequest;
+}) {
+  const chatCtl = chatController;
+
+  const setResponse = React.useCallback((): void => {
+    const res = { type: 'custom', value: 'Good!' };
+    chatCtl.setActionResponse(actionRequest, res);
+  }, [actionRequest, chatCtl]);
+
+  return (
+    <div>
+      <Button
+        type="button"
+        onClick={setResponse}
+        variant="contained"
+        color="primary"
+      >
+        Good!
+      </Button>
+    </div>
+  );
+}
+export default App;
+
+//export default withAuthenticator(App);
